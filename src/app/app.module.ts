9ec434dc4a3f1b6220d6e8d6fee8a8d9cae0cf57
Ricko Virnanda <rickovir@gmail.com>
2018-08-27 01:42:00 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { StartPage } from '../pages/start/start';
+import { DaftarPage } from '../pages/daftar/daftar';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    StartPage,
+    DaftarPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    StartPage,
+    DaftarPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/start/start.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DaftarPage } from '../../pages/daftar/daftar';
+import { LoginPage } from '../../pages/login/login';
+
+/**
+ * Generated class for the StartPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-start',
+  templateUrl: 'start.html',
+})
+export class StartPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad StartPage');
+  }
+
+  goLogin()
+  {
+  	this.navCtrl.setRoot(LoginPage);
+  }
+
+  goDaftar()
+  {
+  	this.navCtrl.setRoot(DaftarPage);
+  }
+
+
+}
